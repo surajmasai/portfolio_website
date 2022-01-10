d11b878dbf0ec590613bd4449a528e77dd734734
SURAJ KUMAR <sksurajkumar7090@gmail.com>
2022-01-11 01:58:16 +0530

    update profile and remove 1 project

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -10,7 +10,7 @@ const Hero = () => (
     <Section row nopadding>
       <LeftSection>
         <SectionTitle main center>
-          <img src="https://media-private.canva.com/3UpVY/MAExsA3UpVY/1/tl.jpg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAJWF6QO3UH4PAAJ6Q%2F20220105%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Date=20220105T035900Z&X-Amz-Expires=72306&X-Amz-Signature=1906ac1a7300e0dfd601d655068f1c68fca60dd8da339237021ce923d2696096&X-Amz-SignedHeaders=host&response-expires=Thu%2C%2006%20Jan%202022%2000%3A04%3A06%20GMT" width="40%" style={{ borderRadius: "50%" }} /> <br></br>
+          <img src="./images/profile.jpg" width="40%" style={{ borderRadius: "50%" }} /> <br></br>
           Hey There!<br />
           I am Suraj Kumar
         </SectionTitle>
